d71c83e3b6aa322ee2505897f44a0b6858098238
Michael Vlaar <michael@vlaar.it>
2018-10-19 16:16:59 +0200

    Changed template to of-watchdog
    Signed-off-by: Michael Vlaar <michael@vlaar.it>

--- pointinpolygon/handler.go
+++ pointinpolygon/handler.go
@@ -1,13 +1,10 @@
 package function
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"encoding/json"
-)
-
-const (
-	Inside  = "inside"
-	Outside = "outside"
-	Error   = "error"
 )
 
 type Location struct {
@@ -20,25 +17,43 @@ type Request struct {
 	Polygon []*Location `json:"polygon"`
 }
 
-// Handle a serverless request
-func Handle(req []byte) string {
-	request := &Request{}
+type Response struct {
+	PointInPolygon bool `json:"pointInPolygon"`
+}
 
-	if err := json.Unmarshal(req, request); err != nil {
-		return Error
+func Handle(w http.ResponseWriter, r *http.Request) {
+	var input []byte
+
+	if r.Body != nil {
+		defer r.Body.Close()
+		body, _ := ioutil.ReadAll(r.Body)
+		input = body
 	}
 
-	inside := false
+	request := &Request{}
+
+	if err := json.Unmarshal(input, request); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	pointInPolygon := false
 	for i, j := 0, len(request.Polygon)-1; i < len(request.Polygon); i = i + 1 {
 		if (((request.Polygon[i].Latitude <= request.Point.Latitude) && (request.Point.Latitude < request.Polygon[j].Latitude)) || ((request.Polygon[j].Latitude <= request.Point.Latitude) && (request.Point.Latitude < request.Polygon[i].Latitude))) && (request.Point.Longitude < (request.Polygon[j].Longitude-request.Polygon[i].Longitude)*(request.Point.Latitude-request.Polygon[i].Latitude)/(request.Polygon[j].Latitude-request.Polygon[i].Latitude)+request.Polygon[i].Longitude) {
-			inside = !inside
+			pointInPolygon = !pointInPolygon
 		}
 		j = i
 	}
 
-	if inside {
-		return Inside
-	} else {
-		return Outside
+	result, err := json.Marshal(&Response{
+		PointInPolygon: pointInPolygon,
+	})
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
 }
