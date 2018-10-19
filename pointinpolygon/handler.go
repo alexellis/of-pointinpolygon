6517458e1418ae0d25a2ae815c6382947a331f6f
Michael Vlaar <michael@vlaar.it>
2018-10-19 15:41:58 +0200

    Added point in polygon algorithm
    Signed-off-by: Michael Vlaar <michael@vlaar.it>

--- pointinpolygon/handler.go
+++ pointinpolygon/handler.go
@@ -1,10 +1,44 @@
 package function
 
 import (
-	"fmt"
+		"encoding/json"
 )
+
+const (
+	Inside = "inside"
+	Outside = "outside"
+	Error = "error"
+)
+
+type Location struct {
+	Latitude float32 `json:"latitude"`
+	Longitude float32 `json:"longitude"`
+}
+
+type Request struct {
+	Point *Location `json:"point"`
+	Polygon []*Location `json:"polygon"`
+}
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. Of-PointInPolygon said: %s", string(req))
+	request := &Request{}
+
+	if err := json.Unmarshal(req, request); err != nil {
+		return Error
+	}
+
+	inside := false
+	for i, j := 0, len(request.Polygon) - 1; i < len(request.Polygon); i = i + 1 {
+		if (((request.Polygon[i].Latitude <= request.Point.Latitude) && (request.Point.Latitude < request.Polygon[j].Latitude)) || ((request.Polygon[j].Latitude <= request.Point.Latitude) && (request.Point.Latitude < request.Polygon[i].Latitude))) && (request.Point.Longitude < (request.Polygon[j].Longitude - request.Polygon[i].Longitude) * (request.Point.Latitude - request.Polygon[i].Latitude) / (request.Polygon[j].Latitude - request.Polygon[i].Latitude) + request.Polygon[i].Longitude) {
+			inside = !inside
+		}
+		j = i
+	}
+
+	if inside {
+		return Inside
+	} else {
+		return Outside
+	}
 }
